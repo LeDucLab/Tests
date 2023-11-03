f5f04bec2bcf069614601a8e49fe683af034b0b4
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2023-11-03 21:35:05 +0100

    app_file.py

--- app_file.py
+++ app_file.py
@@ -25,7 +25,10 @@ if st.button("Zur Vorlage"):
     # Display text based on the selected option
     if option == "Erstberatung Exom":
         st.write("**Beratungsgrund:** V.a. genetisch bedingte XX")
-        st.write("Sehr geehrte/r", Titel, Name,",")
+        if Titel == "Herr"
+        st.write("Sehr geehrter", Titel, Name,",")
+        else
+        st.write("Sehr geehrte", Titel, Name,",")
         st.write("am", current_datetime.strftime('%d.%m.%Y'), "stellten Sie Ihren Sohn/Ihre Tochter  in unserer genetischen Sprechstunde vor.")
         st.write("**Eigenanamnese:**")
         st.write("Sie berichteten, dass XX nach un/auffälliger Schwangerschaft in der Schwangerschaftswoche (per Sectio/als hypotrophes Neugeborenes/mit …) geboren wurde. Ihre/Seine Geburtsmaße betrugen … [Daten aus pedz].")
