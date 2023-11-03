976019047446ff7897a85a0027da9671bb6a35a3
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2023-11-03 16:40:47 +0100

    app_file.py

--- app_file.py
+++ app_file.py
@@ -1,13 +1,21 @@
 import streamlit as st
 
+
 # Create a Streamlit app
-st.title("Button and Text Introduction App")
+st.title("Button and Text Introduction with Options")
+
+# Add a selectbox for choosing an option
+option = st.selectbox("Select an option:", ["Option 1", "Option 2", "Option 3"])
 
 # Create a button
 if st.button("Click me!"):
-    # Display text when the button is clicked
-    st.write("You clicked the button!")
+    # Display text based on the selected option
+    if option == "Option 1":
+        st.write("You chose Option 1!")
+    elif option == "Option 2":
+        st.write("You chose Option 2!")
+    elif option == "Option 3":
+        st.write("You chose Option 3!")
 
 # You can also add more text or content below the button
-st.write("This is some additional text below the button.")
-
+st.write("This is some additional text below the button and options.")
