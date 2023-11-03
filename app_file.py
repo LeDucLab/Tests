a2ada581efc53c9a83e8d609cc17fb1b235db4ca
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2023-11-03 17:13:18 +0100

    app_file.py

--- app_file.py
+++ app_file.py
@@ -15,22 +15,17 @@ option = st.selectbox("Wählen Sie die Art der Beratung", ["Erstberatung Exom", 
 if st.button("Zu Vorlage"):
     # Display text based on the selected option
     if option == "Erstberatung Exom":
-        st.write("**Beratungsgrund**: V.a. genetisch bedingte XX")
+        st.write("**Beratungsgrund:** V.a. genetisch bedingte XX")
         st.write("Sehr geehrte Frau/Herr XX,")
         st.write("am", current_datetime.strftime('%d.%m.%Y'), "stellten Sie Ihren Sohn/Ihre Tochter  in unserer genetischen Sprechstunde vor.")
-      
-     # Eigenanamnese:
-      #Sie berichteten, dass  nach un/auffälliger Schwangerschaft in der Schwangerschaftswoche (per Sectio/als hypotrophes Neugeborenes/mit …) geboren wurde. Ihre/Seine Geburtsmaße betrugen … [Daten aus pedz].
-
-      #Familienanamnese:
-      #Hinsichtlich der aktuellen Fragestellung berichteten Sie, dass bei Unterlagen zu den genannten Familienmitgliedern liegen uns nicht vor. Ein drei Generationen umfassender Stammbaum befindet sich im Anhang.
-      #Sie berichteten keine für die Fragestellung relevanten Krankheitsbilder in Ihrer Familie.
-
-      #Körperliche Untersuchung:
-     # Wir sahen im Alter von  Jahren. Ihre/Seine Körpermaße zur Vorstellung betrugen:[Daten aus pedz]. Fazial ergeben sich keine Auffälligkeiten/Fazial fielen … auf.
-
-     # Beurteilung:
-      #Bei Ihrem Sohn/Ihrer Tochter besteht der Verdacht auf ein . Aus der Sicht unseres Fachgebietes ist eine genetische Diagnostik indiziert. Wir veranlassten daher eine molekulargenetische Paneldiagnostik in den hierfür ursächlichen Genen bei ihm/ihr. Sobald der Befund der genetischen Diagnostik vorliegt, werden wir Sie informieren und weiterführend Stellung nehmen.")
+        st.write("**Eigenanamnese:**"")
+        st.write("Sie berichteten, dass  nach un/auffälliger Schwangerschaft in der Schwangerschaftswoche (per Sectio/als hypotrophes Neugeborenes/mit …) geboren wurde. Ihre/Seine Geburtsmaße betrugen … [Daten aus pedz].")
+        st.write ("**Familienanamnese:**")
+        st.write("Hinsichtlich der aktuellen Fragestellung berichteten Sie, dass bei Unterlagen zu den genannten Familienmitgliedern liegen uns nicht vor. Ein drei Generationen umfassender Stammbaum befindet sich im Anhang. Sie berichteten keine für die Fragestellung relevanten Krankheitsbilder in Ihrer Familie.")
+        st.write("**Körperliche Untersuchung:**")
+        st.write("Wir sahen im Alter von  Jahren. Ihre/Seine Körpermaße zur Vorstellung betrugen:[Daten aus pedz]. Fazial ergeben sich keine Auffälligkeiten/Fazial fielen … auf.")
+        st.write("**Beurteilung:**)
+        st.write("Bei Ihrem Sohn/Ihrer Tochter besteht der Verdacht auf ein . Aus der Sicht unseres Fachgebietes ist eine genetische Diagnostik indiziert. Wir veranlassten daher eine molekulargenetische Paneldiagnostik in den hierfür ursächlichen Genen bei ihm/ihr. Sobald der Befund der genetischen Diagnostik vorliegt, werden wir Sie informieren und weiterführend Stellung nehmen.")
     elif option == "Option 2":
         st.write("You chose Option 2!")
     elif option == "Option 3":
