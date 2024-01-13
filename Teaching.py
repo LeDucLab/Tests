501d1c00c9fb65e2f4e90dd76198da9d2b1eb12f
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2024-01-13 14:40:29 +0100

    Update Teaching.py

--- Teaching.py
+++ Teaching.py
@@ -37,15 +37,15 @@ for question_data in question_data_1:
     st.subheader(f"Frage 1:")
     st.write(question_data['Question'])
     user_answer_1 = st.text_input("Ihre Antwort:", key="user_answer_1")
-    if st.button("Einreichen", key="A1"):
+    if st.button("Einreichen Frage 1", key="A1"):
         if any(word.lower() in question_data['Answer'].lower() for word in st.session_state.user_answer_1.split()):
             st.success("Korrekt!")
             st.markdown(f'<img src="{question_data["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
             for question_data_2 in question_data_2:
                 st.subheader(f"Frage 2:")
                 st.write(question_data_2['Question'])
-                user_answer_2 = st.radio("Ihre Antwort:", options=question_data_2['','Options'], key="user_answer_2")
-                if st.button("Einreichen", key="A2"):
+                user_answer_2 = st.radio("Ihre Antwort:", options=question_data_2['Options'], key="user_answer_2")
+                if st.button("Einreichen Frage 2", key="A2"):
                     if user_answer_2 == question_data_2['Answer']:
                         st.success("Korrekt!")
                         st.markdown(f'<img src="{question_data_2["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
