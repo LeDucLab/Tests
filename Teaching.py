9d15c8ca24092a16ba9b2dfed1d8294a6c1bdabd
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2024-01-13 15:00:56 +0100

    Update Teaching.py

--- Teaching.py
+++ Teaching.py
@@ -44,7 +44,7 @@ for question_data in question_data_1:
             for question_data_2 in question_data_2:
                 st.subheader(f"Frage 2:")
                 st.write(question_data_2['Question'])
-                user_answer_2 = st.radio("Ihre Antwort:", options=question_data_2['Options'], key="user_answer_2")
+                user_answer_2 = st.radio("Ihre Antwort:", options=question_data_2['','Options'], key="user_answer_2")
                 if st.button("Einreichen Frage 2", key="A2"):
                     if user_answer_2 == question_data_2['Answer']:
                         st.success("Korrekt!")
