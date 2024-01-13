956cec4fa87ebebd6461b6c283f2b63b55244a15
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2024-01-13 14:30:05 +0100

    Update Teaching.py

--- Teaching.py
+++ Teaching.py
@@ -41,17 +41,17 @@ for question_data in question_data_1:
         if any(word.lower() in question_data['Answer'].lower() for word in st.session_state.user_answer_1.split()):
             st.success("Korrekt!")
             st.markdown(f'<img src="{question_data["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
-            for question_data in question_data_2:
+            for question_data_2 in question_data_2:
                 st.subheader(f"Frage 2:")
-                st.write(question_data['Question'])
-                user_answer_2 = st.radio("Ihre Antwort:", options=question_data['Options'], key="user_answer_2")
+                st.write(question_data_2['Question'])
+                user_answer_2 = st.radio("Ihre Antwort:", options=question_data_2['Options'], key="user_answer_2")
                 if st.button("Einreichen", key="A2"):
-                    if user_answer_2 == question_data['Answer']:
+                    if user_answer_2 == question_data_2['Answer']:
                         st.success("Korrekt!")
-                        st.markdown(f'<img src="{question_data["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
+                        st.markdown(f'<img src="{question_data_2["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
                     else:
                         st.warning("Falsch! Versuchen Sie nochmal.")
-                        st.markdown(f'<img src="{question_data["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
+                        st.markdown(f'<img src="{question_data_2["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
         else:
             st.warning("Falsch! Versuchen Sie nochmal.")
             st.markdown(f'<img src="{question_data["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
