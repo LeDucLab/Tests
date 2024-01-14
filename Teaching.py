7dcfa27ba09d06d79c4d114bd90df84ee29aa163
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2024-01-14 18:12:27 +0100

    Update Teaching.py

--- Teaching.py
+++ Teaching.py
@@ -80,6 +80,7 @@ elif st.session_state.question_index == 1:
             st.markdown(f'<img src="{current_question["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
             st.session_state.question_index += 1
             st.write(st.session_state.question_index)
+            st.button("Nächste Frage", key="Q3")
         else:
             st.warning("Falsch! Versuchen Sie nochmal.")
             st.markdown(f'<img src="{current_question["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
