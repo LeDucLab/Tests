e4429275c62e72d2d4539be119f036b7e05396f4
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2024-01-14 18:44:09 +0100

    Update Teaching.py

--- Teaching.py
+++ Teaching.py
@@ -103,7 +103,7 @@ elif st.session_state.question_index == 2:
             st.markdown(f'<img src="{current_question["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
 
 elif st.session_state.question_index == 3:
-    st.success("Sie haben die Übung erfolgreich abgeschlossen. Viel Spaß beim Lernen!<br>Liebe Grüße,<br>Diana Le Duc ")
+    st.success("Sie haben die Übung erfolgreich abgeschlossen. Viel Spaß beim Lernen!<br>Liebe Grüße,<br>Diana Le Duc1")
     st.balloons()
 # Display the first question
 #for question_data_1_item in question_data_1:
