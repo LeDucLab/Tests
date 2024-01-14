ee91bd9f520f9e245798e4cdcfe4781f9d0d07a0
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2024-01-14 13:22:45 +0100

    Update Teaching.py

--- Teaching.py
+++ Teaching.py
@@ -33,29 +33,29 @@ question_data_2 = [
 
 
 # Display the first question
-for question_data in question_data_1:
+for question_data_1_item in question_data_1:
     st.subheader(f"Frage 1:")
-    st.write(question_data['Question'])
+    st.write(question_data_1_item['Question'])
     user_answer_1 = st.text_input("Ihre Antwort:", key="user_answer_1")
     if st.button("Einreichen Frage 1", key="A1"):
-        if any(word.lower() in question_data['Answer'].lower() for word in st.session_state.user_answer_1.split()):
+        if any(word.lower() in question_data_1_item['Answer'].lower() for word in st.session_state.user_answer_1.split()):
             st.success("Korrekt!")
-            st.markdown(f'<img src="{question_data["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
-            for question_data_2 in question_data_2:
+            st.markdown(f'<img src="{question_data_1_item"CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
+            for question_data_2_item in question_data_2:
                 st.subheader(f"Frage 2:")
-                st.write(question_data_2['Question'])
-                user_answer_2 = st.radio("Ihre Antwort:", options=question_data_2['Options'], key="user_answer_2")
+                st.write(question_data_2_item['Question'])
+                user_answer_2 = st.radio("Ihre Antwort:", options=question_data_2_item['Options'], key="user_answer_2")
                 #if st.button("Einreichen Frage 2", key="A2"):
                 if user_answer_2 !='':
-                    if user_answer_2 == question_data_2['Answer']:
+                    if user_answer_2 == question_data_2_item['Answer']:
                         st.success("Korrekt!")
-                        st.markdown(f'<img src="{question_data_2["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
+                        st.markdown(f'<img src="{question_data_2_item["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
                     else:
                         st.warning("Falsch! Versuchen Sie nochmal.")
-                        st.markdown(f'<img src="{question_data_2["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
+                        st.markdown(f'<img src="{question_data_2_item"IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
         else:
             st.warning("Falsch! Versuchen Sie nochmal.")
-            st.markdown(f'<img src="{question_data["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
+            st.markdown(f'<img src="{question_data_1_item["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
        
 
 
