c7d8c13bfcd30ea5c20f46901c5690fc399e8fe2
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2024-01-16 03:32:14 +0100

    Update Teaching.py

--- Teaching.py
+++ Teaching.py
@@ -10,17 +10,9 @@ import streamlit as st
 st.title("Personalisierte Medizin: Welche Informationen haben Sie mitgenommen")
 
 # Define the questions
-question_data_1 = [
-    {
-        'Question': 'Welche Informationen müssen berücksichtigt werden, um zu entscheiden, welche Therapie für ein Neugeborenes geeignet ist, bei dem im Rahmen des Neugeborenenscreenings eine spinale Muskelatrophie (SMA) diagnostiziert wurde?',
-        'Answer': 'Kopienzahl vom SMN2-Gen',
-        'CorrectImageURL': 'https://github.com/LeDucLab/Tests/raw/main/Images/SMA_v1.png',
-        'IncorrectImageURL': 'https://github.com/LeDucLab/Tests/raw/main/Images/SMA_v2.png',
-        'QuestionType': 'fill_in',
-    },
-]
 
-question_data_2 = [
+
+question_data_1 = [
     {
         'Question': 'Welche Methode wird voraussichtlich eine Schlüsselrolle in der Zukunft der genetischen Therapie in der personalisierten Medizin spielen?',
         'Options': ['','Gentechnisch veränderte Organismen', 'Bioprinting von Organen', 'Gezielte Genom-Editierung', 'Stammzelltransplantation'],
@@ -31,7 +23,7 @@ question_data_2 = [
     },
 ]
 
-question_data_3 = [
+question_data_2 = [
     {
         'Question': 'Was ist das Hauptziel der personalisierten Medizin?',
         'Options': ['','Eine One-Size-Fits-All-Ansatz für alle Patienten', 'Eine individualisierte Behandlung basierend auf genetischen, molekularen und anderen individuellen Merkmalen', 'Die ausschließliche Verwendung traditioneller Behandlungsansätze', 'Die Maximierung der Kosteneffizienz bei medizinischen Interventionen'],
@@ -42,6 +34,18 @@ question_data_3 = [
     },
 ]
 
+question_data_3 = [
+    {
+        'Question': 'Welche Informationen müssen berücksichtigt werden, um zu entscheiden, welche Therapie für ein Neugeborenes geeignet ist, bei dem im Rahmen des Neugeborenenscreenings eine spinale Muskelatrophie (SMA) diagnostiziert wurde?',
+        'Answer': 'Kopienzahl vom SMN2-Gen',
+        'CorrectImageURL': 'https://github.com/LeDucLab/Tests/raw/main/Images/SMA_v1.png',
+        'IncorrectImageURL': 'https://github.com/LeDucLab/Tests/raw/main/Images/SMA_v2.png',
+        'QuestionType': 'fill_in',
+    },
+]
+
+
+
 
 # Initialize session state
 if 'question_index' not in st.session_state:
@@ -50,18 +54,20 @@ if 'question_index' not in st.session_state:
 
 # Display the current question
 if st.session_state.question_index == 0:
-    current_question = question_data_1[0]
+    current_question = question_data_2[0]
     st.subheader(f"Frage 1:")
     st.write(current_question['Question'])
-    user_answer_1 = st.text_input("Ihre Antwort:", key="user_answer_1")
-    if st.button("Einreichen Frage 1", key="A1"):
-        if any(word.lower() in current_question['Answer'].lower() for word in user_answer_1.split()):
+    user_answer_1 = st.radio("Ihre Antwort:", options=current_question['Options'], key="user_answer_2")
+    if user_answer_1 != '':
+        if user_answer_1 == current_question['Answer']:
             st.success("Korrekt!")
             st.markdown(f'<img src="{current_question["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
             st.session_state.question_index += 1
             st.write(st.session_state.question_index)
-            #button_placeholder.button.on_event('on_click', lambda x: x.js('document.querySelector("button[data-baseweb-id=1]").click()'))
             st.button("Nächste Frage", key="Q2")
+        else:
+            st.warning("Falsch! Versuchen Sie nochmal.")
+            st.markdown(f'<img src="{current_question["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
 
 # Display the next question
         else:
@@ -86,21 +92,18 @@ elif st.session_state.question_index == 1:
             st.markdown(f'<img src="{current_question["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
    
 elif st.session_state.question_index == 2:
-    current_question = question_data_3[0]
+     current_question = question_data_3[0]
     st.subheader(f"Frage 3:")
     st.write(current_question['Question'])
-    user_answer_3 = st.radio("Ihre Antwort:", options=current_question['Options'], key="user_answer_2")
-    if user_answer_3 != '':
-        if user_answer_3 == current_question['Answer']:
+    user_answer_3 = st.text_input("Ihre Antwort:", key="user_answer_1")
+    if st.button("Antwort einreichen", key="A3"):
+        if any(word.lower() in current_question['Answer'].lower() for word in user_answer_1.split()):
             st.success("Korrekt!")
             st.markdown(f'<img src="{current_question["CorrectImageURL"]}" alt="Korrekt" width="100%">', unsafe_allow_html=True)
             st.session_state.question_index += 1
             st.write(st.session_state.question_index)
-            st.button("Belohnung", key="End")
-            
-        else:
-            st.warning("Falsch! Versuchen Sie nochmal.")
-            st.markdown(f'<img src="{current_question["IncorrectImageURL"]}" alt="Falsch" width="100%">', unsafe_allow_html=True)
+            #button_placeholder.button.on_event('on_click', lambda x: x.js('document.querySelector("button[data-baseweb-id=1]").click()'))
+            st.button("Nächste Frage", key="Q4")
 
 elif st.session_state.question_index == 3:
     st.success("Sie haben die Übung erfolgreich abgeschlossen. Viel Spaß beim Lernen! Liebe Grüße, Diana Le Duc")
