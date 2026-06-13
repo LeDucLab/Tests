c31cedfa3dba6add6bb942363811404c458dd774
LeDucLab <149783757+LeDucLab@users.noreply.github.com>
2026-06-13 09:47:06 +0200

    Update variant_fetcher_v2.py

--- variant_fetcher_v2.py
+++ variant_fetcher_v2.py
@@ -117,7 +117,7 @@ if st.button("Retrieve ACMG Information"):
         # -----------------------------
         gene = csq.get("gene_symbol", variant.get("gene_symbol", "UnknownGene"))
         #transcript = csq.get("transcript", csq.get("feature", "NA"))
-        transcript = variant.get("transcript",csq.get("transcript", csq.get("feature", "NA"))
+        transcript = variant.get("transcript",csq.get("transcript", csq.get("feature", "NA")))
 
 
         hgvs_c = csq.get("hgvs_c", "NA")
